ea04191564d14a2ff6a0e523fc103ce7fa72494d
Marios Michalopoulos <michalopoulos.m14@gmail.com>
2021-02-27 14:05:32 +0200

    First commit

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import javax.persistence.*;
 import java.lang.reflect.Constructor;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity //Connect to db
 @Table
@@ -22,24 +23,23 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob; //date of birth
+    @Transient //There is no need to be a column in our database. It will not appear in our db
     private Integer age;
 
     public Student(){
 
     }
-    public Student(Long id, String name, String email, LocalDate dob, Integer age){
+    public Student(Long id, String name, String email, LocalDate dob){
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -59,7 +59,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setId(Long id) {
---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.APRIL;
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                    1L,
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, APRIL, 5)
+
+            );
+            Student alex = new Student(
+
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2005, JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
